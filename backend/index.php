1d891cdb6b582ea9dbcd2384702fa1d0b0e5bbd4
unknown <UDAHAMA@LKSRIWL-H5BLR32.PEROOT.com>
2015-08-10 19:44:24 +0530

    backend index added

---
+++ backend/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Dashboard</title>
+</head>
+
+<body>
+<h1>Admin Portal of Focus Vision</h1>
+
+<hr />
+<a href="album_upload.php" target="new">Create Album</a><br />
+<a href="image_upload.php" target="new">ADD Images to Album</a>
+
+
+
+</body>
+</html>
